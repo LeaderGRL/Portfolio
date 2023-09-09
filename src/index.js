ece6d162297f8214a5d33c2ba973f2d27af9cf68
Jojo <jordangrilly@gmail.com>
2023-09-09 10:21:37 +0200

    Update : Improve responsive cards

--- src/index.js
+++ src/index.js
@@ -5,15 +5,20 @@ const projectsRoutes = require('./api/routes/projectsRoutes');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, _, next) => {
-    console.log(`Incoming request: ${req.method} ${req.path}`);
-    console.log('Headers:', req.headers);
-    console.log('Body:', req.body);
-    next();
+// app.use((req, _, next) => {
+//     console.log(`Incoming request: ${req.method} ${req.path}`);
+//     console.log('Headers:', req.headers);
+//     console.log('Body:', req.body);
+//     next();
+// });
+
+app.use(express.static(__dirname ));
+
+app.get('/', (_, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.use("/api", projectsRoutes);
-
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
